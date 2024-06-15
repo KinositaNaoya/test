fa6d3a766e7dab2117bf307e6624599139516031
KinositaNaoya <kinosita121130@gmail.com>
2024-06-16 03:14:54 +0900

    Log生成

--- Assets/Script/aaaaaaaa.cs
+++ Assets/Script/aaaaaaaa.cs
@@ -7,7 +7,8 @@ public class aaaaaaaa : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // パパラッチ
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
